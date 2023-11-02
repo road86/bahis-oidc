f5db4861f519961bc2eeb0c205730a941870182f
Sharful Islam <34204530+sharful09@users.noreply.github.com>
2023-11-02 13:23:32 +0600

    switch sqlite db to postgresql db
    now the coredb has been used for oidc server. The user table of bahis
    serve is being used for authentication.

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    class Meta:
+        db_table = "auth_user"
+        # abstract = True
